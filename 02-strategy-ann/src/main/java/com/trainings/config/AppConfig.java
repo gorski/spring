7a8045c65c343f440e6999ae90cc5369233038b6
Marcin Górski <mg@mgorski.net>
2015-03-23 10:39:44 +0100

    annotation based config

--- 02-strategy-ann/src/main/java/com/trainings/config/AppConfig.java
+++ 02-strategy-ann/src/main/java/com/trainings/config/AppConfig.java
@@ -1,9 +1,16 @@
 package com.trainings.config;
 
+import com.trainings.basic.CalculationStrategy;
+import com.trainings.basic.ConcreteStrategyAdd;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-   
+    @Bean
+    public CalculationStrategy myStrategy(){
+        return new ConcreteStrategyAdd();
+    }
+
 }
