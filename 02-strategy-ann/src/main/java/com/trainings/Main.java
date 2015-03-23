1d64657346567619d6bb09b88d1aa723608f9ce5
Marcin Górski <mg@mgorski.net>
2015-03-23 10:34:04 +0100

    annotation based config

--- 02-strategy-ann/src/main/java/com/trainings/Main.java
+++ 02-strategy-ann/src/main/java/com/trainings/Main.java
@@ -1,5 +1,6 @@
 package com.trainings;
 
+import com.trainings.basic.CalculationStrategy;
 import com.trainings.basic.ConcreteStrategyAdd;
 import com.trainings.config.AppConfig;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		ConfigurableApplicationContext xmlContext = new AnnotationConfigApplicationContext(AppConfig.class);
-		ConcreteStrategyAdd context = (ConcreteStrategyAdd) xmlContext.getBean("myStrategy");
+		CalculationStrategy context = (CalculationStrategy) xmlContext.getBean("myStrategy");
 		System.out.println("Result (spring): " + context.execute(10, 20));
 	}
 
