8e674edaba09497b52c349f8c226528560c69e1a
Marcin Górski <mg@mgorski.net>
2015-03-23 11:15:22 +0100

    Strategy with factory bean (not working example!)

--- 02-strategy-xml-with-factory/src/main/java/com/trainings/StrategyFactory.java
+++ 02-strategy-xml-with-factory/src/main/java/com/trainings/StrategyFactory.java
@@ -1,12 +1,22 @@
 package com.trainings;
 
+import com.trainings.basic.CalculationStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
 /**
  *
  */
 public class StrategyFactory {
 
-    public StrategyExecutor createStrategy(){
+    @Autowired
+    CalculationStrategy myStrategy;
 
-        return null;
+    public StrategyExecutor createStrategy(){
+        StrategyExecutor executor = new StrategyExecutor();
+        executor.setY(20);
+        executor.setX(324);
+        executor.setCalculationStrategy(myStrategy);
+        return executor;
     }
 }
