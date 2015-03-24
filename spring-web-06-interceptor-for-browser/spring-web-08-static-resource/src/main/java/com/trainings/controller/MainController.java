b6e9a5bd30fddddb0da9ce155126a9d8724f393e
Marcin Górski <mg@mgorski.net>
2015-03-24 10:28:26 +0100

    Static resources example

---
+++ spring-web-06-interceptor-for-browser/spring-web-08-static-resource/src/main/java/com/trainings/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.trainings.controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(MainController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String sayHello() {
+
+        LOG.info("Entering controller method");
+
+        return "/WEB-INF/jsp/hello.jsp";
+    }
+
+
+}
---
+++ spring-web-06-interceptor-for-browser/spring-web-08-static-resource/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Spring application</title>
+    <link rel="stylesheet" type="text/css" href="static/style.css">
+</head>
+<body>
+<h1>Hello!</h1>
+</body>
+</html>
